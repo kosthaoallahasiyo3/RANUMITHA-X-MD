49ae8436921950a80a857db2ddc557904b488bec
kosthaoallahasiyo3 <kosthaoallahasiyo@gmail.com>
2025-08-25 14:25:34 +0530

    Update get-jid.js

--- plugins/get-jid.js
+++ plugins/get-jid.js
@@ -9,24 +9,22 @@ cmd({
     category: "utility",
     filename: __filename,
 }, async (conn, mek, m, { 
-    from,q, isGroup, reply, sender 
+    from, isGroup, isCreator, reply, sender 
 }) => {
     try {
-        if (!q) {
-            // Ensure user JID ends with @s.whatsapp.net
-            const userJID = sender.includes('@s.whatsapp.net') ? sender : `${sender}@s.whatsapp.net`;
-            return reply(`${userJID}`);
-    }
-            
-        if (!isGroup) {
+        if (isGroup) {
             // Ensure group JID ends with @g.us
             const groupJID = from.includes('@g.us') ? from : `${from}@g.us`;
             return reply(`${groupJID}`);
-        } 
-            
+        } else {
+            // Ensure user JID ends with @s.whatsapp.net
+            const userJID = sender.includes('@s.whatsapp.net') ? sender : `${sender}@s.whatsapp.net`;
+            return reply(`${userJID}`);
+        }
 
     } catch (e) {
         console.error("JID Error:", e);
         reply(`⚠️ Error fetching JID:\n${e.message}`);
     }
 });
+
